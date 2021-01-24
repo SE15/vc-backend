508571073eecea33ee00c8a9fdf3430403e6c88a
Thushani-Jayasekera <thushanij.18@cse.mrt.ac.lk>
2021-01-24 23:54:45 +0530

    ER

--- config/database.js
+++ config/database.js
@@ -4,7 +4,9 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_DB, process.env.DB_USER,  process.env.DB_PASS, {
     host: process.env.DB_HOST,
     port: process.env.PORT || 3306,
+    
     dialect: 'mysql'
   });
 
 module.exports=sequelize;
+
