cf2eeb0b98ac5aed9e939309f1e4cbe9de84d1e3
Thushani-Jayasekera <thushanij.18@cse.mrt.ac.lk>
2021-01-24 23:08:17 +0530

    changes-pls check before merge

--- config/database.js
+++ config/database.js
@@ -1,17 +1,9 @@
-//import { createPool } from 'mysql';
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
-//var pool = createPool({
-//    connectionLimit: 10, 
-//    host: process.env.DB_HOST,
-//    user: process.env.DB_USER,
-//    password: process.env.DB_PASS,
-//    database: process.env.DB_DB,
-//});
 
-//TODO: Convert these config variables to env when deploying
-const sequelize = new Sequelize('YDu6zWJ1mT', 'YDu6zWJ1mT',  'r1S3iks9pv', {
-    host: 'remotemysql.com',
-    port: 3306,
+const sequelize = new Sequelize(process.env.DB_DB, process.env.DB_USER,  process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.PORT || 3306,
     dialect: 'mysql'
   });
 
