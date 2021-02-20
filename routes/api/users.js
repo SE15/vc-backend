47775f22857d7b27df2bbf8a41b45a60ad39cf09
Avishka-Rathnavibushana <mailavishka@gmail.com>
2021-02-20 07:52:59 +0530

    correcting routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,15 +7,15 @@ const userController = require('../../controllers/userController');
 //user routes
 router.get('/users/', authorization, userController.searchUser);
 router.get('/users/:id', authorization, userController.viewProfile);
-router.post('/users/skills/add', authorization, userController.addSkill);
+router.post('/users/skills/', authorization, userController.addSkill);
 router.delete('/users/skills/:id', authorization, userController.removeSkill);
 router.put('/users/skills/:id', authorization, userController.validateSkill);
-router.post('/users/connections/add/:id',authorization, userController.addConnection);
-router.put('/users/connections/respond/:id',authorization, userController.respondConnection);
+router.post('/users/connections/:id',authorization, userController.addConnection);
+router.put('/users/connections/:id',authorization, userController.respondConnection);
 router.delete('/users/connections/:id', authorization, userController.removeConnection);
 router.post('/users/recommendations/:id', authorization, userController.submitRecommendation);
 router.delete('/users/', authorization, userController.deleteAccount);
-router.put('/users/changepwd', authorization, userController.changePassword);
-router.put('/users/edit', authorization, userController.editProfile);
+router.put('/users/', authorization, userController.changePassword);
+router.post('/users/', authorization, userController.editProfile);
 
 module.exports = router;
